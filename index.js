82d4278df7529c2624693e076be4bca17e496578
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-01 17:07:54 +0530

    echo server method get otherwise 404

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-var http = require('http');
+/*
+check in postman
+method GET url Post 
+
+*/var http = require('http');
 
 http.createServer(function(request, response) {
-  var body = [];
-  request.on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-    response.end(body);
-  });
+  if (request.method === 'GET' && request.url === '/echo') {
+    var body = [];
+    request.on('data', function(chunk) {
+      body.push(chunk);
+    }).on('end', function() {
+      body = Buffer.concat(body).toString();
+      response.end(body);
+    })
+  } else {
+    response.statusCode = 404;
+    response.end();
+  }
 }).listen(8080);
