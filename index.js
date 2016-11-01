50a069c3ba3bae13f36e83244b8a289962ffd733
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-01 20:34:08 +0530

    hello world

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-/*
-check in postman
-method GET url Post 
-
-*/var http = require('http');
+var http = require('http');
 
 http.createServer(function(request, response) {
-  if (request.method === 'GET' && request.url === '/echo') {
-    var body = [];
-    request.on('data', function(chunk) {
-      body.push(chunk);
-    }).on('end', function() {
-      body = Buffer.concat(body).toString();
-      response.end(body);
-    })
-  } else {
-    response.statusCode = 404;
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+    // At this point, we have the headers, method, url and body, and can now
+    // do whatever we need to in order to respond to this request.
+
+    response.write("hello\n");
+    response.write("from: " + url + "\n");
+    response.write("method: " + method +"\n");
+    response.write("with: " + JSON.stringify(headers));
     response.end();
-  }
-}).listen(8080);
+  });
+}).listen(8080); // Activates this server, listening on port 8080.
+console.log("server running at port 8080");
