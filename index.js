158e176745a4e5bc3ce0ce8b0daaa93066931cce
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-02 11:41:07 +0530

    Content Type text/plain add Pre tag And inside that data

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-/*   hello from URL*/
 
+/*   ob1 ===Content Type text/plain add Pre tag And inside that data 
+     ob2 ===Content Type text/html  treate data as HTML
+  */
 
-var http = require('http');
-
-http.createServer(function(request, response) {
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
-  request.on('error', function(err) {
-    console.error(err);
-  }).on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-    // At this point, we have the headers, method, url and body, and can now
-    // do whatever we need to in order to respond to this request.
-
-    response.write("hello\n");
-    response.write("from: " + url + "\n");
-    response.write("method: " + method +"\n");
-    response.write("with: " + JSON.stringify(headers));
-    response.end();
-  });
-}).listen(8080); // Activates this server, listening on port 8080.
-console.log("server running at port 8080");
+var http = require("http");
+http.createServer(function (req,res) {
+res.writeHead(200,{"Content-Type":"text/html"});
+res.end("<h1>hello</h1>");
+}).listen(8080);
+console.log("server running on 8080");
