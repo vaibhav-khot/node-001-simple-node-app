d9fbeeee483884d9dd5098f4c115970f6ee6b605
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-02 14:47:11 +0530

    Blocking Code Example

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-/*   ob1 ===Content Type text/plain add Pre tag And inside that data 
+/*   ob1 ===Content Type text/plain add Pre tag And inside that data
      ob2 ===Content Type text/html  treate data as HTML
   */
 
-var http = require("http");
-http.createServer(function (req,res) {
-res.writeHead(200,{"Content-Type":"text/html"});
-res.end("<h1>hello</h1>");
-}).listen(8080);
-console.log("server running on 8080");
+  var fs = require("fs");
+
+  var data = fs.readFileSync('input.txt');
+
+  console.log(data.toString());
+  console.log("Program Ended");
